7f5b1e96e6ad0d38692d7f13024132d5daa61ef1
Dima Romanov <this.RomanovDmitry@gmail.com>
2023-07-23 13:06:04 +0300

    Add forgotten class InputAnalyzer

---
+++ src/main/java/ru/romanov/romancalc/utils/InputAnalyzer.java
@@ -0,0 +1,32 @@
+package ru.romanov.romancalc.utils;
+
+
+public class InputAnalyzer {
+
+    public static boolean isLastInputSymbolIsAction(String input) {
+        if (input.isEmpty()) return false;
+        char lastInputChar = input.charAt(input.length() - 1);
+        return lastInputChar == '+' || lastInputChar == '-' || lastInputChar == ':' || lastInputChar == '×';
+    }
+
+    public static boolean isTextFieldContainsAction(String input) {
+        return input.contains("+") || input.contains("-") || input.contains(":") || input.contains("×");
+    }
+
+    public static boolean isBothNumbersAlreadyInput(String input) {
+        String inputAction = getInputAction(input);
+        char lastChar = input.charAt(input.length() - 1);
+        if (inputAction.isEmpty() || inputAction.charAt(0) == lastChar) {
+            return false;
+        }
+        return true;
+    }
+
+    static String getInputAction(String input) {
+        if      (input.contains("-")) return "-";
+        else if (input.contains("+")) return "+";
+        else if (input.contains(":")) return ":";
+        else if (input.contains("×")) return "×";
+        else return "";
+    }
+}
